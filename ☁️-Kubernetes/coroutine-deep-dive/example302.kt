7a7cba2b3fde5420c63b34fc80d1e61d7705cb86
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2025-10-22 18:22:00 +0900

    backup: 2025-10-22 18:22:00 M ☁️-Kubernetes/coroutine-deep-dive/example302.kt

--- ☁️-Kubernetes/coroutine-deep-dive/example302.kt
+++ ☁️-Kubernetes/coroutine-deep-dive/example302.kt
@@ -3,18 +3,22 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 private val executor = Executors.newSingleThreadScheduledExecutor {
-    Thread(it, "scheduler").apply { isDaemon = true } // isDaemon 의 뜻: 백그라운드에서 실행되는 스레드
+    Thread(it, "scheduler").apply { isDaemon = true } // isDaemon 의 뜻: 백그라운드에서 실행되는 스레드.
+    // 스레드를 사용하기는 하지만, delay
+}
+
+suspend fun delay(timeMillis: Long) {
+    suspendCoroutine<Unit> { continueIt ->
+        executor.schedule({
+            continueIt.resume(Unit)
+        }, timeMillis, java.util.concurrent.TimeUnit.MILLISECONDS)
+    }
 }
 
 suspend fun main() {
     println("Start")
 
-    suspendCoroutine<Unit> { continueIt ->
-        executor.schedule({
-            println("Hello, World!")
-            continueIt.resume(Unit)
-        }, 3, java.util.concurrent.TimeUnit.SECONDS)
-    }
+    delay(2000L)
 
     println("End")
 }
