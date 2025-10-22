675f9c1ecb830c1e2a96b8edd6ae4c41dc75f476
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2025-10-22 22:23:07 +0900

    backup: 2025-10-22 22:23:07 A ☁️-Kubernetes/coroutine-deep-dive/.gitattributes ☁️-Kubernetes/coroutine-deep-dive/.gitignore ☁️-Kubernetes/coroutine-deep-dive/bu1/Example101.kt ☁️-Kubernetes/coroutine-deep-dive/build.gradle.kts ☁️-Kubernetes/coroutine-deep-dive/gradle.properties ☁️-Kubernetes/coroutine-deep-dive/gradle/libs.versions.toml ☁️-Kubernetes/coroutine-deep-dive/gradle/wrapper/gradle-wrapper.jar ☁️-Kubernetes/coroutine-deep-dive/gradle/wrapper/gradle-wrapper.properties ☁️-Kubernetes/coroutine-deep-dive/settings.gradle.kts, D ☁️-Kubernetes/coroutine-deep-dive/bu2/build.gradle.kts ☁️-Kubernetes/coroutine-deep-dive/bu2/settings.gradle.kts, R ☁️-Kubernetes/coroutine-deep-dive/gradlew ☁️-Kubernetes/coroutine-deep-dive/gradlew.bat

---
+++ ☁️-Kubernetes/coroutine-deep-dive/bu1/Example101.kt
@@ -0,0 +1,27 @@
+package bu1
+
+import kotlin.concurrent.thread
+
+fun main() {
+    repeat(100_000) {
+        thread {
+            Thread.sleep(1000L)
+            print(".")
+        }
+    }
+
+    repeat(100_000) {
+        launch {
+            delay(100L)
+            print(".")
+        }
+    }
+}
+
+//fun main() = runBlocking {
+//    repeat(100_000) {
+//        launch {
+//
+//        }
+//    }
+//}
--- ☁️-Kubernetes/coroutine-deep-dive/bu2/settings.gradle.kts
+++
@@ -1,7 +0,0 @@
-rootProject.name = "coroutine-deep-dive"
-
-pluginManagement {
-    plugins {
-        kotlin("jvm") version "1.9.21"
-    }
-}
