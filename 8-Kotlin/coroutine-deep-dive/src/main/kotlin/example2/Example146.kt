2bc248be1ba348072d87e25f3b6a834158c463e3
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2025-10-28 23:28:48 +0900

    backup: 2025-10-28 23:28:48 M 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example146.kt 8-Kotlin/kotlin-corouties-deep-dive.md

--- 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example146.kt
+++ 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example146.kt
@@ -1,10 +1,13 @@
 package example2
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
-suspend fun main() = coroutineScope {
+suspend fun main() = coroutineScope(
+) {
+    Dispatchers.Default.limitedParallelism(5)
     repeat(1000) {
         launch {
             List(1000) { Random.nextInt() }.maxOrNull()
