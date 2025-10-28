84ba36ce9edae6f6867a8013b6a15d87a1e40191
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2025-10-28 20:44:46 +0900

    backup: 2025-10-28 20:44:46 A 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example1102.kt

---
+++ 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example1102.kt
@@ -0,0 +1,24 @@
+package example2
+
+import kotlinx.coroutines.*
+
+// pg. 130
+suspend fun main() = runBlocking(CoroutineName("PARENT")) { // name
+    println("Before")
+    longTask()
+    println("After")
+}
+
+suspend fun longTask() = coroutineScope {
+    launch {
+        delay(1000L)
+        val name = coroutineContext[CoroutineName]?.name
+        println("[$name] Finished task 1")
+    }
+    launch {
+        delay(2000L)
+        val name = coroutineContext[CoroutineName]?.name
+        println("[$name] Finished task 2")
+    }
+    // 여기서 멈추고있는거같음.
+}
