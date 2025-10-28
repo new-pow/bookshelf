582e0ba5d079efbc20b432ff6f38c99c91c7a99e
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2025-10-28 23:46:18 +0900

    backup: 2025-10-28 23:46:18 M 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example146.kt 8-Kotlin/kotlin-corouties-deep-dive.md

--- 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example146.kt
+++ 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example146.kt
@@ -1,15 +1,17 @@
 package example2
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
+@OptIn(ExperimentalCoroutinesApi::class)
 suspend fun main() = coroutineScope(
 ) {
-    Dispatchers.Default.limitedParallelism(5)
+    val dis = Dispatchers.Default.limitedParallelism(3)
     repeat(1000) {
-        launch {
+        launch(dis) {
             List(1000) { Random.nextInt() }.maxOrNull()
 
             val threadName = Thread.currentThread().name
