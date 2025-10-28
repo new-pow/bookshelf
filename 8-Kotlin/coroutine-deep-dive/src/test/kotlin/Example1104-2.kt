b6d0ab38c34dddbb609d1a190dafffdffc0532e0
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2025-10-28 22:23:30 +0900

    backup: 2025-10-28 22:23:30 A 8-Kotlin/coroutine-deep-dive/src/test/kotlin/Example1104-2.kt, M 8-Kotlin/coroutine-deep-dive/src/test/kotlin/Example1104.kt

--- 8-Kotlin/coroutine-deep-dive/src/test/kotlin/Example1104-2.kt
+++ 8-Kotlin/coroutine-deep-dive/src/test/kotlin/Example1104-2.kt
@@ -2,7 +2,6 @@ import example1.delay
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.TimeoutCancellationException
-import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeout
@@ -10,20 +9,25 @@ import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 
 // p.138
-class Example1104Test {
+class Example11042Test {
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun testTime2() = runTest {
-        withTimeout(1000L) {
-            advanceTimeBy(900L) // 가상시간 진행
+        withContext(Dispatchers.Default.limitedParallelism(1)) {
+            withTimeout(1000L) {
+                delay(900L)
+            }
         }
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun testTime3() = runTest {
         assertThrows<TimeoutCancellationException> {
-            withTimeout(1000L) {
-                delay(1100L)
+            withContext(Dispatchers.Default.limitedParallelism(1)) {
+                withTimeout(1000L) {
+                    delay(1100L)
+                }
             }
         }
 
