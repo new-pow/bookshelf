7c00620a8783890ef7fb0c490a332f78b0a82afd
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2025-10-29 18:33:30 +0900

    backup: 2025-10-29 18:33:30 M 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example156.kt

--- 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example156.kt
+++ 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example2/Example156.kt
@@ -1,7 +1,22 @@
 package example2
 
-import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.*
 import java.util.concurrent.Executors
+import kotlin.coroutines.coroutineContext
 
 val LoomDispatcher = Executors.newVirtualThreadPerTaskExecutor()
     .asCoroutineDispatcher()
+
+// 확장
+val Dispatchers.Loom: CoroutineDispatcher
+    get() = LoomDispatcher
+
+suspend fun main() {
+    coroutineScope {
+        repeat(1_000_000) {
+            launch(Dispatchers.Default) {
+                Thread.sleep(1000)
+            }
+        }
+    }.let(::println)
+}
