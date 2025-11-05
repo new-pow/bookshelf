b069ff2a7d31f53ebb9456e74c371cc60634f3a3
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2025-11-05 11:12:11 +0900

    backup: 2025-11-05 11:12:11 M 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example3/Example226.kt, A 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example3/Example271.kt

---
+++ 8-Kotlin/coroutine-deep-dive/src/main/kotlin/example3/Example271.kt
@@ -0,0 +1,82 @@
+package example3
+
+import kotlinx.coroutines.delay
+
+// 1. 기본 람다식
+//fun main() {
+//    val f : () -> Unit = {
+//        println("A")
+//        println("B")
+//        println("C")
+//    }
+//
+//    f()
+//    f()
+//}
+
+// 2. suspend
+//suspend fun main() {
+//    val f : suspend () -> Unit = {
+//        println("A")
+//        delay(2000L)
+//        println("B")
+//        delay(2000L)
+//        println("C")
+//        delay(2000L)
+//    }
+//
+//    f()
+//    f()
+//}
+
+// 3. emit
+//suspend fun main() {
+//    val f: suspend ((String) -> Unit) -> Unit = { emit ->
+//        emit("A") // emit는 매개변수로 전달된 람다식
+//        emit("B")
+//        emit("C")
+//    }
+//
+//    f { println(it) }
+//    f { println(it) }
+//}
+
+// 4. 중단함수가 되어야함.
+//fun interface FlowCollector { // 함수형 인터페이스
+//    suspend fun emit(value: String)
+//}
+//
+//suspend fun main() {
+//    val f: suspend (FlowCollector) -> Unit = {
+//        it.emit("A")
+//        it.emit("B")
+//        it.emit("C")
+//    }
+//
+//    f { println(it) } // 람다식을 FlowCollector 타입으로 전달
+//    f { println(it) }
+//}
+
+// 5. flowCollector
+fun interface FlowCollector { // 함수형 인터페이스
+    suspend fun emit(value: String)
+}
+
+interface Flow {
+    suspend fun collect(collector: FlowCollector)
+}
+
+suspend fun main() {
+    val builder: suspend FlowCollector.() -> Unit = {
+        emit("A")
+        emit("B")
+        emit("C")
+    }
+    val flow: Flow = object : Flow {
+        override suspend fun collect(collector: FlowCollector) {
+            collector.builder()
+        }
+    }
+
+    flow.collect { println(it) }
+}
